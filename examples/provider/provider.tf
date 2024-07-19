5e4ee0b7b58f7ce8618b52382ead256228256f58
Joshua Tracy <55331456+Josh-Tracy@users.noreply.github.com>
2024-07-19 14:08:40 -0400

    docs(update-docs): Update example docs for the registry

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,1 +1,29 @@
 provider "epic" {}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "epic_random_name" "movie_name" {
+  media_type = "movie"
+  title      = "lord of the rings"
+}
+
+
+resource "epic_random_quote" "lotr_quote" {
+  media_type = "movie"
+  title      = "lord of the rings"
+}
+
+resource "aws_s3_bucket" "epic" {
+  bucket = epic_random_name.movie_name.name
+
+  tags = {
+    Name        = epic_random_name.movie_name.name
+    Description = epic_random_quote.lotr_quote.quote
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.epic.bucket
+}
